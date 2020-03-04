8f1627bf1024c210b6f679d82276ec8e18cdf542
Nino Chimento <saninoc84@gmail.com>
2020-03-04 13:21:16 +0100

    cro pagina show w funziona manualmente con id 1

---
+++ show/server_show.php
@@ -0,0 +1,25 @@
+<?php
+include __DIR__ ."/../account.php";
+$idStanza = $_GET["id"];
+echo "$idStanza";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn && $conn->connect_error) {
+echo "Connection failed: " . $conn->connect_error;
+}
+$sql = "SELECT * FROM stanze Where id = $idStanza";
+$result = $conn->query($sql);
+if ($result && $result->num_rows > 0) {
+// output data of each row
+$rooms=[];
+while($row = $result->fetch_assoc()) {
+
+$rooms[]= $row;
+}
+} elseif ($result) {
+echo "0 results";
+} else {
+echo "query error";
+}
+$conn->close();
+var_dump($rooms);
+ ?>
