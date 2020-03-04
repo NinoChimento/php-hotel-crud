e68758bafbf3a62ba1663ab1d41415d8ec4e009c
Nino Chimento <saninoc84@gmail.com>
2020-03-04 14:38:04 +0100

    modifica lista

--- show/show.php
+++ show/show.php
@@ -12,14 +12,14 @@
     <div class="container">
       <div class="row">
         <div class="col-12">
-          <ul>
+          <ul class="list-group">
             <?php foreach ($rooms as $stanza) {?>
-              <li> Id:<?php echo $stanza["id"]; ?></li>
-              <li>room_number:<?php echo $stanza["room_number"]; ?></li>
-              <li>floor:<?php echo $stanza["floor"]; ?></li>
-              <li>beds:<?php echo $stanza["beds"]; ?></li>
-              <li>created_at:<?php echo $stanza["created_at"]; ?></li>
-              <li>updated_at:<?php echo $stanza["updated_at"]; ?></li>
+              <li class="list-group-item"> Id:<?php echo $stanza["id"]; ?></li>
+              <li class="list-group-item">room_number:<?php echo $stanza["room_number"]; ?></li>
+              <li class="list-group-item">floor:<?php echo $stanza["floor"]; ?></li>
+              <li class="list-group-item">beds:<?php echo $stanza["beds"]; ?></li>
+              <li class="list-group-item">created_at:<?php echo $stanza["created_at"]; ?></li>
+              <li class="list-group-item">updated_at:<?php echo $stanza["updated_at"]; ?></li>
             <?php } ?>
 
           </ul>
