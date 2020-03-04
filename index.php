e8882aaf352d04278255aa775dc514283d632471
Nino Chimento <saninoc84@gmail.com>
2020-03-04 13:41:48 +0100

    creo form per la cancellazione

--- index.php
+++ index.php
@@ -29,7 +29,12 @@
                 <td> <?php echo $stanza["floor"] ?></td>
                 <td><a href="show/show.php?id=<?php echo $stanza["id"] ?>">Vedi</a> </td>
                 <td><a href="#">Uptade</a> </td>
-                <td></td>
+                <td>
+                  <form class="" action="delete/delete.php" method="post">
+                    <input type="hidden" name="id" value="<?php echo $stanza["id"] ?>">
+                    <input type="submit" name="" value="DELETE">
+                </form>
+              </td>
               </tr>
             <?php } ?>
           <?php } ?>
