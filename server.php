74459257ce2b6f0442049762500ff189a4e2731b
Nino Chimento <saninoc84@gmail.com>
2020-03-04 12:51:11 +0100

    var dump di array stanze

---
+++ server.php
@@ -0,0 +1,23 @@
+<?php
+include __DIR__ ."/account.php";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn && $conn->connect_error) {
+echo "Connection failed: " . $conn->connect_error;
+}
+$sql = "SELECT id, room_number, floor FROM stanze";
+$result = $conn->query($sql);
+if ($result && $result->num_rows > 0) {
+// output data of each row
+$rooms=[];
+while($row = $result->fetch_assoc()) {
+
+$rooms[]= $row;
+}
+} elseif ($result) {
+echo "0 results";
+} else {
+echo "query error";
+}
+$conn->close();
+var_dump($rooms);
+ ?>
