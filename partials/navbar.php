e9c17f1d11815c49cac15430778ee16161de37cb
Nino Chimento <saninoc84@gmail.com>
2020-03-05 15:40:43 +0100

    inserimento bottone inner

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,6 +1,7 @@
 <?php
   include __DIR__."/account.php"
  ?>
- <nav class="navbar grey navbar-light">
+ <nav class="navbar bg-info navbar-light">
    <a class="navbar-brand navbar navbar-light bg-light text-danger" href="http://localhost/php-hotel-crud/index.php">hotel Boolean</a>
+   <a class="btn btn-outline-dark"href="#"> Inserisci</a>
  </nav>
---
+++ server/account.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "hotels";
+
+$local ="localhost/";
+$cartella = "php-hotel-crud/";
+$directory = $local.$cartella."index.php";
+echo $directory;
+?>
