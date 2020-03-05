41b720fa8c6746d15cfff895f372e06a2d62e887
Nino Chimento <saninoc84@gmail.com>
2020-03-05 18:50:05 +0100

    inserimento data in creazione nuova stanza

--- inner/inner.php
+++ inner/inner.php
@@ -7,7 +7,7 @@
   <form class="form-group" action="inner_server.php" method="post">
     <div class="form-group">
       <label for="">Numero stanza</label>
-      <input class="form-control" type="number" name="room_number" value="3">
+      <input class="form-control" type="number" name="room_number" value="">
     </div>
     <div class="form-group">
       <label for="Numero piano">Numero piano</label>
@@ -16,6 +16,14 @@
     <div class="form-group">
       <label for="Numero Letti">Numero posti letto</label>
       <input class="form-control" type="number" name="beds" value="">
+    </div>
+    <div class="form-group">
+      <label for="data creazione">Data Arrivo</label>
+      <input class="form-control" type="date" name="data_create" value="">
+    </div>
+    <div class="form-group">
+      <label for="data creazione">Data Partenza</label>
+      <input class="form-control" type="date" name="data_partenza" value="">
     </div>
     <input type="hidden" name="id" value="">
     <input class="btn-danger form-control" type="submit" name="Salva" value="salva">
--- inner/inner_server.php
+++ inner/inner_server.php
@@ -3,6 +3,9 @@
   $roomNumber = $_POST["room_number"];
   $roomFloor =$_POST["floor_number"];
   $roomBeds = $_POST["beds"];
+  $dataArrive  =$_POST["data_create"];
+  $dataDeparture = $_POST["data_partenza"];
+  echo $dataArrive;
   if (empty($roomNumber)) {
     die("devi riempire tutti i campi");
   }
@@ -12,7 +15,7 @@
   if (empty($roomBeds)) {
     die("devi riempire tutti i campi");
   }
-  $sql = "INSERT INTO stanze (room_number,floor,beds) VALUES ('$roomNumber','$roomFloor','$roomBeds')";
+  $sql = "INSERT INTO stanze (room_number,floor,beds,created_at,updated_at) VALUES ('$roomNumber','$roomFloor','$roomBeds','$dataArrive','$dataDeparture')";
   $result = $conn->query($sql);
   header("location:http://localhost/php-hotel-crud/index.php");
  ?>
