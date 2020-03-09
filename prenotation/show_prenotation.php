1632f3e1de9012d074406de929cc1c686c66850a
Nino Chimento <saninoc84@gmail.com>
2020-03-09 13:57:41 +0100

    stampo array prenotazione

--- prenotation/show_prenotation.php
+++ prenotation/show_prenotation.php
@@ -1,7 +1,7 @@
 <?php
   include_once __DIR__."/../function.php";
   $idStanza = $_GET["id"];
-  $sql= "SELECT prenotazioni.id, stanze.room_number,ospiti.name,ospiti.lastname
+  $sql = "SELECT prenotazioni.id, stanze.room_number,ospiti.name,ospiti.lastname
 FROM `prenotazioni`
 INNER JOIN stanze
 ON prenotazioni.stanza_id = stanze.id
@@ -9,5 +9,11 @@ INNER JOIN prenotazioni_has_ospiti
 ON prenotazioni.id = prenotazioni_has_ospiti.prenotazione_id
 INNER JOIN ospiti
 ON prenotazioni_has_ospiti.ospite_id = ospiti.id
-WHERE prenotazioni.id = 1"
+WHERE prenotazioni.id = 1";
+  $result = $conn->query($sql);
+  if ($result && $result->num_rows > 0) {
+  $prenotazione=$result->fetch_assoc();
+  }
+  var_dump($prenotazione);
+
 ?>
